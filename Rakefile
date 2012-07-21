a44e0af2a6f81554beb53290a8cfe216801b954f
Vivek Patel <Vvick.727@gmail.com>
2012-07-21 01:34:25 -0400

    Style changes, joystick in place.

--- Rakefile
+++ Rakefile
@@ -3,17 +3,6 @@ task :hint do
 	sh "./node_modules/.bin/jshint ./js/"
 end
 
-desc "Concatinates and minifies the relevant javascript files into ./static/pup.js"
-task :js do
-	src_dir = "#{pwd}/js"
-	dest_dir = "#{pwd}/static"
-
-	# grab all the files to concatinate
-	# cmd = sprocketize --directory=#{src_dir} -I dummy.js #{to_sprocketize.join(" ")}"
-	# sh "#{cmd} > #{dest_dir}/quest.js"
-	# sh "#{cmd} | uglifyjs > #{dest_dir}/quest.min.js"
-end
-
 desc "Start Sinatra"
 task :start do
 	sh "ruby quest.rb"
--- public/js/qs-load.js
+++ public/js/qs-load.js
@@ -4,3 +4,26 @@ var run = function(code) {
 	var js = CoffeeScript.compile(code);
 	eval(js);
 }
+
+
+(function(Joystick) {
+	var setJoystickValue = function(e) {
+		JoyStick.set(e.newVal % 360);
+	}
+
+	// Load the joystick control
+	// using MAX & MIN a bit of a hack to
+	// get around the forced dial.
+	YUI().use('dial', function(Y) {
+		var dial = new Y.Dial({
+			stepsPerRevolution : 360,
+			value : 0,
+			min : Number.MIN_VALUE,
+			max : Number.MAX_VALUE,
+			after : {
+				valueChange: Y.bind(setJoystickValue, dial)
+			}
+		});
+		dial.render("#joystick");
+	});
+}(Joystick));
