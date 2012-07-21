d163cad69e0e8a99feb2729b5024fa3b41979ceb
Devon Peticolas <devon@branch.com>
2012-07-21 02:11:39 -0400

    added coffeescript rendering

--- app.rb
+++ app.rb
@@ -1,33 +1,36 @@
-#
 # Questscript
 # 
 # app.rb
 # Sinatra server for Questscript
 #
 # Built by Vivek Patel and Devon Peticolas
-#  
 
 require 'sinatra'
 require 'mongo'
-require 'coffee-script'
 
 # DATABASE CONNECTION ---------------------------------
 #@conn	= Mongo::Connection.new
 #@db		= @conn['questscript']
 #@coll	= @db['games']
 
+# RENDERING -------------------------------------------
+
+set :views, File.dirname(__FILE__) + '/assets'
+get '/javascripts/*.js' do
+  filename = "coffeescript/#{params[:splat].first}"
+  coffee filename.to_sym
+end
 
 # HELPERS ---------------------------------------------
 def check(code)
 	# TODO: Perform check
-	return true
+	true
 end
 
 # Error saving
 error 500 do
 	"Error in attempting to save"
 end
-
 
 # ROUTES ----------------------------------------------
 # Main handler
