96aa104b194ef93f6bd2ef7d6531ebab1ab87f8d
Vivek Patel <Vvick.727@gmail.com>
2012-07-21 04:23:06 -0400

    steez.

--- assets/coffeescript/Joystick.coffee
+++ assets/coffeescript/Joystick.coffee
@@ -7,16 +7,30 @@ Joystick = (->
 
 	# public methods -----------------
 	pub = {}
+
+	# sets the cardinal direction depending
+	# on a code (mapped to arrow key codes)
 	pub.setDirection = (code) ->
 		switch code
-			when 38
-				direction = UP
 			when 37
 				direction = LEFT
-			when 40
-				direction = DOWN
+			when 38
+				direction = UP
 			when 39
 				direction = RIGHT
+			when 40
+				direction = DOWN
+
+	# sets direction based on degree
+	pub.setDegree = (degree) ->
+		if degree > 245 and degree < 295
+			pub.setDirection 37   # LEFT 
+		else if degree > 335 or degree < 25
+			pub.setDirection 38   # UP
+		else if degree > 65 and degree < 115
+			pub.setDirection 39   # RIGHT 
+		else if degree > 155 and degree < 205
+			pub.setDirection 40   # DOWN
 	
 	pub.getDirection = ->
 		direction
--- public/js/qs-load.js
+++ public/js/qs-load.js
@@ -5,24 +5,76 @@ var run = function(code) {
 	eval(js);
 }
 
+$("button[name='run']").click(function() {
+	var code = $('#editor-form').val();
+	run(code);
+});
 
+
+var Joystick;
 (function(Joystick) {
-	var setJoystickValue = function(e) {
-		JoyStick.set(e.newVal % 360);
-	}
+	var dial,
+		INTENT_THRESH = 50,
+		fastPoll = 100,
+		
+		getPositions = function(target) {
+			var handle = target._handleNode._node,
+				marker = target._markerNode._node,
+				handleX = parseInt(handle.style.left.substring(0, handle.style.left.length-2), 10),
+				handleY = parseInt(handle.style.top.substring(0, handle.style.top.length-2), 10),
+				markerX = parseInt(marker.style.left.substring(0, marker.style.left.length-2), 10),
+				markerY = parseInt(marker.style.top.substring(0, marker.style.top.length-2), 10);
+			return {
+				handleX : handleX,
+				handleY : handleY,
+				markerX : markerX,
+				markerY : markerY,
+				deltaX  : Math.abs(handleX - markerX),
+				deltaY  : Math.abs(handleY - markerY)
+			};
+		},
+		
+		interval = setInterval(function() {
+			if (dial) {
+				var p = getPositions(dial);
+				if (Math.sqrt(p.deltaX * p.deltaX +
+						p.deltaY * p.deltaY) < INTENT_THRESH) {
+					// TODO: UNCOMMENT LATER	
+					// Joystick.setIntent(false);
+				} else {
+					// TODO: UNCOMMENT LATER
+					// Joystick.setIntent(true);
+				}
+			}
+		}, fastPoll),
+		
+		setJoystickValue = function(e) {
+			var currentTarget = e.currentTarget;
+
+			// TODO: UNCOMMENT LATER
+			//Joystick.setDegree(currentTarget._angle);
+		};
 
 	// Load the joystick control
 	// using MAX & MIN a bit of a hack to
 	// get around the forced dial.
 	YUI().use('dial', function(Y) {
-		var dial = new Y.Dial({
+		dial = new Y.Dial({
 			stepsPerRevolution : 360,
 			value : 0,
-			min : Number.MIN_VALUE,
-			max : Number.MAX_VALUE,
+			diameter : 100,
+			handleDistance : 0,
+			handleDiameter : 0.5,
+			strings : { 
+				label: "",
+				resetStr : "" 
+			},
 			after : {
-				valueChange: Y.bind(setJoystickValue, dial)
-			}
+				valueChange : Y.bind(setJoystickValue, dial)
+			},
+			centerButtonDiameter : 0,
+			min : -Number.MAX_VALUE,
+			max : Number.MAX_VALUE
 		});
 		dial.render("#joystick");
 	});
